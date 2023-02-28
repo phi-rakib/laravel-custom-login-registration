688c310883832349ee6fd4a5a22cc2f4014e2a69
rakib <phi.rakib@gmail.com>
2023-02-28 16:20:42 +0600

    added routes for login & registration

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(AuthController::class)->group(function() {
+    Route::get('login', 'index')->name('login');
+    Route::post('post-login', 'postLogin')->name('login.post');
+    Route::get('registration', 'registration')->name('registration');
+    Route::post('post-registration', 'postRegistration')->name('registration.post');
+    Route::get('dashboard', 'dashboard')->name('dashboard');
+    Route::get('logout', 'logout')->name('logout');
 });
